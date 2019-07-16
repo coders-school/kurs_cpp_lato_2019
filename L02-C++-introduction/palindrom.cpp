83e4210c1701e8939858b5964fbced58a049d515
piotrkowandy <piotr.kowandy@o2.pl>
2019-07-16 21:19:47 +0200

    palindrom changed, comment to file added

--- L02-C++-introduction/palindrom.cpp
+++ L02-C++-introduction/palindrom.cpp
@@ -2,11 +2,24 @@
 
 bool palindrom(std::string napis)
 {
-    std::size_t dlugosc = napis.size();
-    for(std::size_t i = 0; i < (dlugosc / 2); ++i)
+    std::float dlugosc = napis.size();
+    for(std::float i = 0; i < (dlugosc / 2); ++i)
     {
         if(napis[i] != napis[dlugosc - (i + 1)])
             return false;
     }
     return true;
 }
+
+
+/*
+error: conversion to ‘float’ from ‘std::__cxx11::basic_string<char>::size_type {aka long unsigned int}’ may alter its value [-Werror=conversion]
+
+wywolanie funkcji size() na zmiennej napis zwraca nam dlugosc napisu, 
+ktory jest typu log unsigned int, wiec jesli chcemy aby dlugosc zostala przypisana
+do zmiennej o typie float, musimy sie liczyc z pewna utrata danych.
+
+Zatem odradzalbym uzywanie float w tym kontekscie.
+
+
+*/
