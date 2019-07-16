796d50965bb87f448828500ba3eea2375545362d
Szymon Gajewski <szymongaju@gmail.com>
2019-07-16 22:06:30 +0200

    std:: added to string_view

--- L02-C++-introduction/palindrom_recursive.cpp
+++ L02-C++-introduction/palindrom_recursive.cpp
@@ -1,10 +1,10 @@
 #include <string>
 
-bool palindrom(string_view napis)
+bool palindrom(std::string_view napis)
 {
     if(napis.size() < 2) return true;
 
-    string_view sv = napis;
+    std::string_view sv = napis;
 
     return napis.front() == napis.back()
         && palindrom(sv.substr(1, napis.size() - 2));
