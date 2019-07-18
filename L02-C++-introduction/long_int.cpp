744742033267b3ef445a415f1e7558216fb64a8f
piotrkowandy <piotr.kowandy@o2.pl>
2019-07-18 23:34:10 +0200

    Rest of tasks: default, bug, -=, -, +, <<, >>

--- L02-C++-introduction/long_int.cpp
+++ L02-C++-introduction/long_int.cpp
@@ -5,6 +5,9 @@
 
 struct LongInt
 {
+    //Constructor with default keyword let us to create object without specific variables inside (see variable val3, val4)
+    LongInt() = default;
+
     //Constructor
     LongInt(int left, int right) : left(left), right(right)
     {
@@ -34,17 +37,172 @@ struct LongInt
     //Method
     std::string toString()
     {
-        return std::to_string(left) + std::to_string(right);
+	if (right < 10 && right > -10)
+	{
+	    if (left != 0)
+	    {
+		if (right > 0)
+		{
+                    return std::to_string(left*10) + std::to_string(right);
+		}
+		else
+		{
+		    return std::to_string(left*10) + std::to_string(right * -1);
+		}
+	    }
+	    else
+	    {
+		return std::to_string(right);
+	    }
+	}
+	else
+	{
+	    if (left != 0)
+	    {
+		if (right > 0)
+		{
+	    	    return std::to_string(left) + std::to_string(right);
+		}
+		else
+		{
+		    return std::to_string(left) + std::to_string(right * -1);
+		}
+	    }
+	    else
+	    {
+		return std::to_string(right);
+	    }
+	}
     }
 
-    //Operator
+    //Operator +=
     void operator+=(const LongInt& r)
     {
-        left += r.left;
+	left += r.left;
         right += r.right;
         left += right / 100;
         right = right % 100;
     }
+
+    //Operator -=
+    void operator-=(const LongInt& r)
+    {
+
+	if (r.left > 0)
+	{
+	    if (left > 0)
+	    {
+	    	right -= r.right;
+	    	left -= r.left;
+	    }
+	    else
+	    {
+		right -= r.right;
+		left -= r.left;
+		left += right / 100;
+		right = right % 100;
+	    }
+	}
+	else
+	{
+	    right += (r.right*-1);
+	    left += (r.left * -1);
+            left += right / 100;
+	    right = right % 100;
+
+	}
+
+	if (right < 0 && left > 0)
+	{
+	    right += 100;
+	    left -= 1;
+	}
+	else
+	{
+	    if (right < 0)
+	    {
+		right *= -1;	   
+	    }
+	}
+    }
+
+    //Operator +
+    LongInt operator+(const LongInt& other)
+    {
+	LongInt another;
+	another.left = left + other.left;
+	another.right = right + other.right;
+	another.left += another.right / 100;
+	another.right = another.right % 100;
+	return another;
+    }
+
+    //Operator -
+    LongInt operator-(const LongInt& other)
+    {
+	LongInt another;
+
+	if (other.left > 0)
+	{
+	    if (left > 0)
+	    {
+	    	another.right = right - other.right;
+	    	another.left = left - other.left;
+	    }
+	    else
+	    {
+		another.right = right - other.right;
+		another.left = left - other.left;
+		another.left = another.left + another.right / 100;
+		another.right =  another.right % 100;
+	    }
+	}
+	else
+	{
+	    another.right = right + (other.right*-1);
+	    another.left = left + (other.left * -1);
+            another.left = another.left + another.right / 100;
+	    another.right = another.right % 100;
+
+	}
+
+	if (another.right < 0 && another.left > 0)
+	{
+	    another.right += 100;
+	    another.left -= 1;
+	}
+	else
+	{
+	    if (another.right < 0)
+	    {
+		another.right *= -1;	   
+	    }
+	}
+
+	return another;
+    }
+
+    
+    //Operator <<
+    friend std::ostream& operator<<(std::ostream& output, const LongInt& other)
+    {
+	output << other.left << other.right;
+	return output;
+    }
+   
+   //Operator >>
+   friend std::istream& operator>>(std::istream& input,  LongInt& other)
+   {
+	std::cout << std::endl << "Give me left parametr: ";
+	input >>  other.left;
+	std::cout << "Give me right parametr: ";
+	input >> other.right;
+
+	other.left += other.right / 100;
+	other.right = other.right % 100;
+
+	return input;
+   } 
 
     //Copy operator
     LongInt& operator=(const LongInt& other)
@@ -63,7 +221,57 @@ int main()
 {
     LongInt val1(1, 70);
     LongInt val2("5980");
+    LongInt val3;
+    LongInt val4;
+    LongInt val5(0, 10);
+    LongInt val6(0, 0);
+    LongInt val7(30, 1);
 
-    val1 += val2;
-    std::cout << val1.toString() << std::endl;
+    LongInt valTest1(12, 4);
+    LongInt valTest2(8,2);
+    LongInt valTest3(7, -9);
+    LongInt valX(0,99);
+    LongInt valY(1,1);
+    LongInt val90(-30, -1);
+
+    std::cout<<"test for val6(0, 0) -> "<< val6.toString() << std::endl;
+    std::cout<<"test for val5(0, 10) -> "<< val5.toString() << std::endl;
+    std::cout<<"test for val7(30, 1) -> "<< val7.toString() << std::endl;
+    std::cout<<"test for valTest1(12, 4) -> "<< valTest1.toString() << std::endl;
+    std::cout<<"test for valTest2(8, 2) -> "<< valTest2.toString() << std::endl;
+    std::cout<<"test for valTest3(7, -9) -> " << valTest3.toString() << std::endl;
+
+    std::cout<<"test for val90(-30, -1)" <<val90.toString() << std::endl;
+    std::cout<<std::endl;
+
+    LongInt val10("-340");
+    LongInt val11("5690");
+    std::cout<<"Operator -= : " << val10.toString() << " -= " << val11.toString() <<" = ";
+    val10 -= val11;
+    std::cout<< val10.toString() << std::endl << std::endl;
+
+    std::cout<<"Operator -= : " << val1.toString() << " -= " << val2.toString() <<" = ";
+    val1 -= val2;
+    std::cout<< val1.toString() << std::endl << std::endl;
+
+    std::cout<<"Operator -= : " << valTest1.toString() << " -= " << valTest2.toString() <<" = ";
+    valTest1 -= valTest2;
+    std::cout<< valTest1.toString() << std::endl << std::endl;
+
+    LongInt val95("340");
+    LongInt val96("5690");
+    std::cout<<"Operator - : " << val95.toString() << " - " << val96.toString() <<" = ";
+    val3 = val95 - val96;
+    std::cout<< val3.toString() << std::endl << std::endl;
+
+    std::cout<<"Operator + : " << valX.toString() << " + " << valY.toString() <<" = ";
+    val3 = valX + valY;
+    std::cout<< val3.toString() << std::endl << std::endl;
+
+    std::cout<<"Operator << : ";
+    std::cout<< val2.toString() << std::endl << std::endl;
+
+    std::cout<<"Operator >> : ";
+    std::cin >> val4;
+    std::cout<< val4.toString() << std::endl << std::endl;
 }
