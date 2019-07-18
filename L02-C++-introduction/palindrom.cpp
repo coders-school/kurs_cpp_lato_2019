c4676581b63a9de621c33fa556938f041935f399
Piotr Hoffner <piotr.hoffner.wroc@gmail.com>
2019-07-18 11:47:47 +0200

    float inaccuracy on large values

--- L02-C++-introduction/palindrom.cpp
+++ L02-C++-introduction/palindrom.cpp
@@ -1,12 +1,33 @@
 #include <string>
+#include <iostream>
 
 bool palindrom(std::string napis)
 {
-    std::size_t dlugosc = napis.size();
-    for(std::size_t i = 0; i < (dlugosc / 2); ++i)
+    float dlugosc = napis.size();
+    for(float i = 0; i < (dlugosc / 2); ++i)
     {
         if(napis[i] != napis[dlugosc - (i + 1)])
             return false;
     }
     return true;
 }
+
+int main()
+{
+    std::cout << std::boolalpha << palindrom("kajak") << std::endl;
+    std::cout << std::boolalpha << palindrom("aaaaaaaaaaaaaaaaaaaa") << std::endl;
+    std::cout << std::boolalpha << palindrom("onomatopeja") << std::endl;
+    std::string longString(1000000000,'X');
+    std::cout << std::boolalpha << palindrom(longString) << std::endl;
+    return 0;
+}
+
+// OUTPUT:
+// palindrom("kajak"):                  true
+// palindrom("aaaaaaaaaaaaaaaaaaaa"):   true
+// palindrom("onomatopeja"):            false
+// palindrom(longString):               false 
+
+// palindrom(longString) returned unexpected false value
+// due to inaccuracy of floating-point variable on large numbers
+// float dlugosc = napis.size()
