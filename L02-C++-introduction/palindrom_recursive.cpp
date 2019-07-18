54fc3fcc3a0b17d4f8d8966655289af234cc05b4
Piotr Hoffner <piotr.hoffner.wroc@gmail.com>
2019-07-18 12:47:07 +0200

    palindrom recursive function using std::string_view

--- L02-C++-introduction/palindrom_recursive.cpp
+++ L02-C++-introduction/palindrom_recursive.cpp
@@ -1,9 +1,20 @@
-#include <string>
+#include <string_view>
+#include <iostream>
 
-bool palindrom(std::string napis)
+bool palindrom(std::string_view napis)
 {
     if(napis.size() < 2) return true;
 
     return napis.front() == napis.back()
         && palindrom(napis.substr(1, napis.size() - 2));
+}
+
+int main()
+{
+    std::cout << std::boolalpha << palindrom("kajak") << std::endl;
+    std::cout << std::boolalpha << palindrom("aaaaaaaaaaaaaaaaaaaa") << std::endl;
+    std::cout << std::boolalpha << palindrom("onomatopeja") << std::endl;
+    std::string longString(1000000000,'X');
+    std::cout << std::boolalpha << palindrom(longString) << std::endl;
+    return 0;
 }
