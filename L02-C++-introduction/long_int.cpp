51e83da3f8067172301bc82fc1009cbbe476112d
R H <a@a>
2019-07-19 19:17:31 +0200

    operators + - -=

--- L02-C++-introduction/long_int.cpp
+++ L02-C++-introduction/long_int.cpp
@@ -3,16 +3,22 @@
 #include <string>
 #include <cassert>
 
+using namespace std;
 struct LongInt
 {
+
     //Constructor
     LongInt(int left, int right) : left(left), right(right)
     {
+        cout<<"wywolanie konstruktora: "<<endl;
+        cout<<left<<" __ ";
+        cout<<right <<endl;
     }
 
     //Constructor
     LongInt(std::string str)
     {
+        
         int number = std::stoi(str);
         left = number / 100;
         right = number % 100;
@@ -23,6 +29,7 @@ struct LongInt
     {
         left = other.left;
         right = other.right;
+
     }
 
     //Destructor
@@ -45,15 +52,56 @@ struct LongInt
         left += right / 100;
         right = right % 100;
     }
+    LongInt& operator+(const LongInt & r)
+    {
+        (*this)+=r;
+        return *this;
+    }
+    LongInt operator-=(const LongInt& r)
+    {
+      *this=*this-r;
+       return *this;
+    }
 
     //Copy operator
-    LongInt& operator=(const LongInt& other)
+    LongInt& operator=( LongInt& other)
     {
         left = other.left;
         right = other.right;
         return *this;
     }
 
+    LongInt& operator -(const LongInt& r)
+    {
+        int num1, num2;
+        if(this->left<0)
+            num1= this->left* (100) - this->right;
+	else
+             num1= this->left* (100) + this->right;
+        if(r.left<0)
+            num2= r.left* (100) - r.right;
+        else
+            num2= r.left* (100) + r.right;
+            cout<< num1<< " - "<< num2<<"= "<<endl;
+        num1=num1-num2;
+        cout<<num1<<endl;
+        if(num1>0)
+        {
+            this->left = num1 / 100;
+            this->right = -num1 % 100;
+        }
+        if(num1<=-100)
+        {
+            this->left = num1 / 100;
+            this->right = -num1 % 100;
+        }
+        if(num1>-100 && num1<0)
+        {
+            this->left = 0;
+            this->right = num1 % 100;
+        }
+        return *this;
+    }
     //Members
     int left;
     int right;
@@ -61,9 +109,13 @@ struct LongInt
 
 int main()
 {
-    LongInt val1(1, 70);
-    LongInt val2("5980");
 
-    val1 += val2;
-    std::cout << val1.toString() << std::endl;
+    LongInt val1("3001");
+ //   LongInt val2(1,2);
+   // val1=val1-val2;
+   // cout<<val1.left<<val1.right<<endl;
+
+  //  val1 = val2+val1;
+    cout<<val1.left<<val1.right<<endl;
+    //std::cout << val1.toString() << std::endl;
 }
