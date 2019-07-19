d29ad1fbd3930e181bc0f8ed78e0ff7b333f047d
R H <a@a>
2019-07-19 19:59:21 +0200

    palindrom_recursive

--- L02-C++-introduction/palindrom_recursive.cpp
+++ L02-C++-introduction/palindrom_recursive.cpp
@@ -1,9 +1,17 @@
 #include <string>
-
-bool palindrom(std::string napis)
+#include <string_view>
+#include <iostream>
+using namespace std;
+bool palindrom(std::string_view napis)
 {
     if(napis.size() < 2) return true;
 
     return napis.front() == napis.back()
         && palindrom(napis.substr(1, napis.size() - 2));
+}
+
+int main()
+{
+	cout<<palindrom("ala");
+	return 0;
 }
