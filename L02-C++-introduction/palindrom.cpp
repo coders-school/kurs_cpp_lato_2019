b12e8ea46c5f6d10356d57f34313238f5322971c
teojdb <joasia.bednarska90@gmail.com>
2019-07-19 19:17:13 +0200

    Zadanie 1

--- L02-C++-introduction/palindrom.cpp
+++ L02-C++-introduction/palindrom.cpp
@@ -1,12 +1,36 @@
 #include <string>
+#include <iostream>
+
+using namespace std;
+
 
 bool palindrom(std::string napis)
 {
-    std::size_t dlugosc = napis.size();
-    for(std::size_t i = 0; i < (dlugosc / 2); ++i)
+    float dlugosc = napis.size();
+    for(float i = 0; i < (dlugosc / 2); ++i)
     {
         if(napis[i] != napis[dlugosc - (i + 1)])
             return false;
     }
     return true;
 }
+
+
+int main()
+    {
+    string w;
+    cout << "Wpisz wyraz: " << endl;
+    cin >> w;
+
+    if (palindrom(w) == true) cout << "Wyraz jest palindromem!" << endl;
+    else cout << "Wyraz NIE jest palindromem" << endl;
+
+    return 0;
+    }
+
+// Zamiana size_t na float powoduje pojawienie się ostrzeżenia podczas kompilacji z flagą –Wconversion. Kompilator podpowiada, że zachodzi konwersja zmiennej o typie zmiennoprzecinkoym na typ całkowity.
+
+// implicit conversion turns floating-point number into integer: 'float' to'size_type' (aka 'unsigned long') [-Wfloat-conversion
+
+//Size_t przechowuje maksymalny rozmiar obiektu o dowolnym typie i jest szczególnie stosowany w iteracjach w pętli i przy indeksowaniu w tablicach. Używanie float do iteracji w pętli może spowodować niepoprawną liczbę iteracji ze względu na problemy z precyzją, które się kumulują wraz z kolejnymi iteracjami, np. zamiast 11, mamy 10.99908 i jakaś instukcja może się nie wykonać.
+
