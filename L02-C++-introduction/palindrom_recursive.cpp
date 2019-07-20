6245186b2faf9ecaf6a77cb0961b644342cf327d
Daniel Tarnawski <djtarnawski@gmail.com>
2019-07-20 17:39:09 +0200

    Zad.2

--- L02-C++-introduction/palindrom_recursive.cpp
+++ L02-C++-introduction/palindrom_recursive.cpp
@@ -1,9 +1,23 @@
 #include <string>
+#include <string_view>
+#include <iostream>
+bool palindrom(std::string_view sv)
+{	
+    if(sv.size() < 2) return true;
 
-bool palindrom(std::string napis)
+    return sv.front() == sv.back()
+        && palindrom(sv.substr(1, sv.size() - 2));
+}
+
+int main()
 {
-    if(napis.size() < 2) return true;
+	std::string napis;
+	std::cout<<"Write word to check. ";
+        std::cin>>napis;
 
-    return napis.front() == napis.back()
-        && palindrom(napis.substr(1, napis.size() - 2));
+	if(palindrom(napis))
+		std::cout<<"It's palindrome. \n";
+
+	else
+		std::cout<<"It isn't palindrome. \n";
 }
