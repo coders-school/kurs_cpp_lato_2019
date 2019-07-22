3bb19be6e6a048f45a553b89edc7831b5200a300
TheDude-cpu <dudek.u.sylwia@gmail.com>
2019-07-22 17:17:47 +0200

    first commit for palindrom.cpp

--- L02-C++-introduction/palindrom.cpp
+++ L02-C++-introduction/palindrom.cpp
@@ -1,12 +1,32 @@
 #include <string>
+#include <iostream>
+#include <cstdlib>
+
+std::string napis;
 
 bool palindrom(std::string napis)
+
 {
-    std::size_t dlugosc = napis.size();
-    for(std::size_t i = 0; i < (dlugosc / 2); ++i)
+    float dlugosc = napis.size();
+    for(float i = 0; i < (dlugosc / 2); ++i)
     {
         if(napis[i] != napis[dlugosc - (i + 1)])
             return false;
     }
     return true;
 }
+
+
+int main()
+{
+ 	napis = "kajak";
+	std::cout << palindrom(napis)<<std::endl;
+
+	return 0;
+}
+
+// bład konwesji 
+// conversion to ‘float’ from ‘std::__cxx11::basic_string<char>::size_type {aka long unsigned int}’ may alter its value [-Wconversion]
+    // float dlugosc = napis.size();
+    // size_t słuzy do przechowywania maksymalnego rozmiaru obiektu, uzywany przedewszystkim do indeksowania tablic oraz inkrementacji petli
+    // natomiast float słuzy do reperezentacji liczb zeminnoprzecinkowych 
