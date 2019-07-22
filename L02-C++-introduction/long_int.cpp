c8d2d2d6df9e9ed482fc142fb07de5b52e7094bd
AlicjaBonder <alicjabonder@o2.pl>
2019-07-22 19:41:43 +0200

    resolve bug and add stream operator

--- L02-C++-introduction/long_int.cpp
+++ L02-C++-introduction/long_int.cpp
@@ -2,10 +2,26 @@
 #include <iostream>
 #include <string>
 #include <cassert>
+#include <sstream>
 
 void testLongInt();
 struct LongInt
 {
+    friend std::ostream &operator<<(std::ostream &out, const LongInt &c)
+    {
+        out << c.left << c.right;
+        return out;
+    }
+
+    friend std::istream &operator>>(std::istream &in, LongInt &c)
+    {
+        std::cout << "Enter Left Part ";
+        in >> c.left;
+        std::cout << "Enter Right Part ";
+        in >> c.right;
+        return in;
+    }
+
     //Constructor
     LongInt(int left, int right) : left(left), right(right)
     {
@@ -30,7 +46,7 @@ struct LongInt
         std::cout << "Delete " << toString() << std::endl;
     }
 
-    std::string toString()
+    std::string toString() const
     {
 
         if (left)
@@ -41,7 +57,7 @@ struct LongInt
             }
             else
             {
-                return std::to_string(left) + "0";
+                return std::to_string(left) + std::to_string(right);
             }
         }
         else
@@ -59,10 +75,18 @@ struct LongInt
     }
     void operator-=(const LongInt &r)
     {
+     
+        if(right < r.right)
+        {
+            right += 100;
+            left -=1;
+            left -= r.left;
+            right -= r.right;
+        }else{
         left -= r.left;
         right -= r.right;
-        left -= right / 100;
-        right = right % 100;
+        }
+
     }
 
     LongInt operator+(const LongInt &r)
@@ -80,8 +104,18 @@ struct LongInt
     LongInt operator-(const LongInt &r)
     {
         int tLeft, tRight;
+        tRight = right = r.right;
+        if(tRight < 0)
+        {
+            tRight += 100;
+            tLeft = left + r.left -1;
+        }else
+        {
+            tLeft = left - r.left;
+        }
+        
 
-        tLeft = this->left - r.left;
+        tLeft -= this->left - r.left;
         tRight = this->right - r.right;
         tLeft -= tRight / 100;
         tRight = tRight % 100;
@@ -110,4 +144,17 @@ void testLongInt()
     LongInt val2("5980");
     val1 += val2;
     assert(val1.toString() == "6150");
+
+    LongInt valStr("1110");
+    LongInt val3(11, 00);
+    LongInt test;
+    LongInt test2;
+
+    test = val3 + valStr;
+    assert(test.toString() == "2210");
+    val3 -= valStr;
+    assert(val3.toString() == "-10");
+    test2 = val3 - valStr;
+    assert(test2.toString() == "-10");
+
 }
