572a75c77091959cdb8728298ca71a2f82e298f0
Marcin Bury <marcinburyit@gmail.com>
2019-07-24 14:17:53 +0200

    Repaired tostring function

---
+++ L02-C++-introduction/task4.cpp
@@ -0,0 +1,70 @@
+#include <cassert>
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+struct LongInt {
+private:
+  bool isLessThan100() const { return left == 0; }
+
+  bool makeDecimalNumber0() const { return left != 0 && right < 10; }
+
+public:
+  // Constructor
+  LongInt(int left, int right) : left(left), right(right) {}
+
+  // Constructor
+  LongInt(std::string str) {
+    int number = std::stoi(str);
+    left = number / 100;
+    right = number % 100;
+  }
+
+  // Copy constructor
+  LongInt(const LongInt &other) {
+    left = other.left;
+    right = other.right;
+  }
+
+  // Destructor
+  ~LongInt() { std::cout << "Delete " << toString() << std::endl; }
+
+  // Method
+  std::string toString() {
+    if (isLessThan100())
+      return std::to_string(right);
+
+    if (makeDecimalNumber0())
+      return std::to_string(left) + "0" + std::to_string(right);
+
+    return std::to_string(left) + std::to_string(right);
+  }
+
+  // Operator
+  void operator+=(const LongInt &r) {
+    left += r.left;
+    right += r.right;
+    left += right / 100;
+    right = right % 100;
+  }
+
+  // Copy operator
+  LongInt &operator=(const LongInt &other) {
+    left = other.left;
+    right = other.right;
+    return *this;
+  }
+
+  // Members
+  int left;
+  int right;
+};
+
+int main() {
+
+  LongInt val1("3"), val2("1571"), val3("15567");
+
+  std::cout << val1.toString() << std::endl;
+  std::cout << val2.toString() << std::endl;
+  std::cout << val3.toString() << std::endl;
+}
