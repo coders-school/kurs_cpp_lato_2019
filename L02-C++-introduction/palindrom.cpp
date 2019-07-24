01d2252ff1da3b95c8ab1d5a26a6e63d849a3290
Marcin Bury <marcinburyit@gmail.com>
2019-07-24 11:59:05 +0200

    Typ float niedokladny dla dużych wartosci

--- L02-C++-introduction/palindrom.cpp
+++ L02-C++-introduction/palindrom.cpp
@@ -1,12 +1,36 @@
 #include <string>
+#include <iostream>
 
-bool palindrom(std::string napis)
+using namespace std;
+
+bool palindrom(string napis)
 {
-    std::size_t dlugosc = napis.size();
-    for(std::size_t i = 0; i < (dlugosc / 2); ++i)
+    float dlugosc = napis.size();
+    for(float i = 0; i < (dlugosc / 2); ++i)
     {
         if(napis[i] != napis[dlugosc - (i + 1)])
             return false;
     }
     return true;
 }
+
+int main(){
+
+   string longString(500000000,'X');
+    cout << boolalpha << palindrom("manam")        <<endl;
+    cout << boolalpha << palindrom("gggggggggggg") <<endl;
+    cout << boolalpha << palindrom("marcin")       <<endl;
+    cout << boolalpha << palindrom(longString)     <<endl;
+
+    return 0;
+}
+
+//Result:                                                 output:
+//  cout << boolalpha << palindrom("manam")                   true 
+//  cout << boolalpha << palindrom("gggggggggggg")            true
+//  cout << boolalpha << palindrom("marcin")                  fale
+//  cout << boolalpha << palindrom(longString)                false
+
+
+//palidrom(longString) zwraca nieoczekiwaną wartość - False zamiast true
+//typ float jest niedokładna dla dużych liczb
