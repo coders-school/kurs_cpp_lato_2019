a5eb991c267abf5759e74c2065521e894f1ffb01
Lucas W <lukaszwalicki.walus@gmail.com>
2019-07-25 17:07:03 +0200

    homework - palindrom.cpp

--- L02-C++-introduction/palindrom.cpp
+++ L02-C++-introduction/palindrom.cpp
@@ -2,11 +2,17 @@
 
 bool palindrom(std::string napis)
 {
-    std::size_t dlugosc = napis.size();
-    for(std::size_t i = 0; i < (dlugosc / 2); ++i)
+    float dlugosc = napis.size();
+    for(float i = 0; i < (dlugosc / 2); ++i)
     {
         if(napis[i] != napis[dlugosc - (i + 1)])
             return false;
     }
     return true;
 }
+
+
+//zamiana std::size_t na float może zakonczyć się błędem programu,
+//następuje konwersja typów. Po włączeniu -Wconversion plik się
+//nie skompiluje. Float posiada większy zakres, przy przypisaniu
+//do long uinta może nastąpić zmiana wartości.
