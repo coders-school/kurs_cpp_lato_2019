60e7657146fb67013d716c782adce6f4f389cbb3
NataliaG <glowka.natalia@gmail.com>
2019-07-28 22:55:01 +0200

    string_view in palindrom_recursive.cpp

--- L02-C++-introduction/palindrom_recursive.cpp
+++ L02-C++-introduction/palindrom_recursive.cpp
@@ -1,9 +1,20 @@
-#include <string>
+#include <string_view>
+#include <iostream>
 
-bool palindrom(std::string napis)
+bool palindrom(std::string_view napis)
 {
     if(napis.size() < 2) return true;
 
     return napis.front() == napis.back()
         && palindrom(napis.substr(1, napis.size() - 2));
+}
+
+int main()
+{
+    std::string testOK1("kobylamamalybok");
+    std::string testOK2(200, 'N');
+    std::cout << testOK1 << "-" << palindrom(testOK1) << std::endl;
+    std::cout << "200x'N' - " << palindrom(testOK2) << std::endl;
+
+    return 0;
 }
