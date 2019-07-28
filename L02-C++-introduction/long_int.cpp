fbd91fb567506ac4e871411180816207b24341f6
NataliaG <glowka.natalia@gmail.com>
2019-07-29 00:32:10 +0200

    add operators +, -, -=

--- L02-C++-introduction/long_int.cpp
+++ L02-C++-introduction/long_int.cpp
@@ -18,6 +18,9 @@ struct LongInt
         right = number % 100;
     }
 
+    //default constructor
+    LongInt() = default;
+    
     //Copy constructor
     LongInt(const LongInt& other) = default;
 
@@ -65,6 +68,42 @@ struct LongInt
         return *this;
     }
 
+    LongInt operator+(const LongInt& r)
+    {
+        LongInt ret;
+        ret.right = this->right + r.right;
+        ret.left = this->left + r.left;
+        ret.left += ret.right / 100;
+        ret.right = ret.right % 100;
+        return ret;
+    }
+
+    LongInt operator-(const LongInt& r)
+    {
+        LongInt ret;
+        
+        if ((this->right < r.right) && (this->left > 0))
+        {
+            this->left--;
+            this->right += 100;
+        }        
+        ret.right = this->right - r.right;
+        ret.left = this->left - r.left;
+        return ret;
+    }
+
+    void operator-=(const LongInt& r)
+    {
+            
+        if ((right < r.right) && (left > 0))
+        {
+            left--;
+            right += 100;
+        }        
+        right = right - r.right;
+        left = left - r.left;
+    }
+
     //Members
     int left;
     int right;
@@ -85,15 +124,35 @@ int main()
     LongInt val8("3001");
     LongInt val9(30, 1);
 
-    std::cout << val4.toString() << std::endl;
-    std::cout << val5.toString() << std::endl;
-    std::cout << val6.toString() << std::endl;
-    std::cout << val7.toString() << std::endl;
-    std::cout << val8.toString() << std::endl;
-    std::cout << val9.toString() << std::endl;
+    // std::cout << val4.toString() << std::endl;
+    // std::cout << val5.toString() << std::endl;
+    // std::cout << val6.toString() << std::endl;
+    // std::cout << val7.toString() << std::endl;
+    // std::cout << val8.toString() << std::endl;
+    // std::cout << val9.toString() << std::endl;
 
     LongInt val3(val2);
     std::cout << val3.toString() << std::endl;
+
+    LongInt val10 = val2 + val6; //5990
+    std::cout << val10.toString() << std::endl;
+
+    val1 = val9 - val7;
+    std::cout << val1.toString() << std::endl;  //2991
+    val1 = val5 - val7;
+    std::cout << val1.toString() << std::endl;  //-10
+    val1 = val2 - val6;
+    std::cout << val1.toString() << std::endl;  //5970
+
+    val1 = val6 - val8;
+    std::cout << val1.toString() << std::endl;  //left substract doesn't work properly (should be 2991, but is -3009) -> to be fixed
+
+    val9 -= val7;
+    std::cout << val9.toString() << std::endl;  //2991
+    val5 -= val7;
+    std::cout << val5.toString() << std::endl;  //-10
+    val2 -= val6;
+    std::cout << val2.toString() << std::endl;  //5970
 
     return 0;
 }
