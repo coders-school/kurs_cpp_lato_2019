305be3abf831e2ad42a0d7af6edb47b2ec86be0f
darkassazi <kamil.waszkiewicz.2@gmail.com>
2019-08-03 21:48:12 +0200

    Update palindrom.hpp

--- L02-C++-introduction/palindrom.hpp
+++ L02-C++-introduction/palindrom.hpp
@@ -4,8 +4,15 @@ bool palindrom(std::string napis)
 {
     std::size_t dlugosc = napis.size();
     
-    // operacje na  liczbach zmiennoprzecinkowych sa obarczone bledem ktory jest spowodowany nie dokladnoscia oblczen procesora CPU
-    for(std::size_t i = 0; i < (dlugosc / 2); ++i)
+    /*
+    W naszym przypadku wykorzystujemy znak logiczny mniejszy większy i operujemy na liczbach pełnych 
+    co też powoduje że nie zauważyłem żadnych problemów. 
+    
+    float nie powinien być jednak  używany w pętlach. 
+    Jest to podyktowane tym, że liczby zmiennoprzecinkowe nie mogą być dokładnie przedstawione w wartościach binarnych.  
+
+    */
+    for(float i = 0; i < (dlugosc / 2); ++i)
     {
         if(napis[i] != napis[dlugosc - (i + 1)])
             return false;
