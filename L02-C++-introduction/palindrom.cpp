6d431766ca41b2964d9ffbd942b5b8bf89e1f664
alicjaliQui <alicjaliqui@gmail.com>
2019-08-06 21:40:24 +0200

    checked size_t -> float + comment

--- L02-C++-introduction/palindrom.cpp
+++ L02-C++-introduction/palindrom.cpp
@@ -10,3 +10,34 @@ bool palindrom(std::string napis)
     }
     return true;
 }
+
+
+/*
+
+******************************* OUTPUT ************************************
+ palindrom.cpp: In function ‘bool palindrom(std::__cxx11::string)’:
+ palindrom.cpp:5:31: warning: conversion to ‘float’ from 
+ ‘std::__cxx11::basic_string<char>::size_type {aka long unsigned int}’ 
+ may alter its value [-Wconversion]
+     float dlugosc = napis.size();
+                     ~~~~~~~~~~^~
+ palindrom.cpp:8:19: warning: conversion to 
+ ‘std::__cxx11::basic_string<char>::size_type {aka long unsigned int}’ 
+ from ‘float’ may alter its value [-Wfloat-conversion]
+         if(napis[i] != napis[dlugosc - (i + 1)])
+                   ^
+ palindrom.cpp:8:38: warning: conversion to 
+ ‘std::__cxx11::basic_string<char>::size_type {aka long unsigned int}’ 
+ from ‘float’ may alter its value [-Wfloat-conversion]
+         if(napis[i] != napis[dlugosc - (i + 1)])
+
+*****************************************************************************
+
+Po zmianie size_t na float nastepuje niejawna konwersja typu long unsigned int -> float.
+Typ float jest typem zmiennoprzecinkowym, nie adaje sie do tego zastosowania aczkolwiek
+moze dzialac poprawnie. Dopiero wlaczenie flagi -Wconversion wskazalo niepoprawne uzycie
+typu. Dodatkowo typ float ma 4 bajty, a size_t w moim przypadku to 8 bajtow, co oznacza,
+ze przy baaardzo dlugich palindromach float nie sprawdzilby sie tak jak size_t.
+
+
+ */
