492ad375505c4109e4effa5f9152af8f340e313a
piotrkowandy <piotr.kowandy@o2.pl>
2019-08-07 10:33:12 +0200

    Decryption and encryption functions and files added.

--- L04-stl/12A.cpp
+++ L04-stl/12A.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <ctime>
 #include <cstdlib>
-
+#include <fstream>
 
 void fillMap(std::map<char, int>& myMap, std::string& myString)
 {
@@ -40,11 +40,33 @@ int main ()
 	srand(time(NULL));
 	int key = (rand()%10)+1;
 
+	std::string secretString="";
+	std::ifstream myFile ("toEncrypt.txt");
+	if (myFile.is_open())
+	{
+		getline(myFile, secretString);
+		myFile.close();
+	}
+	else
+	{
+		std::cout<< "I'm unable open this file." <<std::endl;
+	}
+
 	std::map<char, int> myMap;
-	std::string secretString {"abcde"}; // string will be downloaded from txt file
 
 	std::cout<< "Orignal sentence: " << secretString << std::endl;
 	std::cout<< "Sentense after encryption: " << encrypt(myMap, key, secretString) << std::endl;
+
+	std::ofstream myFileDe ("decrypt.txt");
+	if (myFileDe.is_open())
+	{
+		myFileDe << secretString << std::endl;
+	}
+	else
+	{
+		std::cout<< "There is a problem with file, i can't write into." << std::endl;
+	}
+
 	std::cout<< "Sentense after decryption: " << decrypt(myMap, key, secretString) << std::endl;
 	
 	return 0;
