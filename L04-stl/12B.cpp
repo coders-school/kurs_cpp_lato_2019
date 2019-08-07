f2d27401a3820d862588354a6bb7ac75700e6e7a
piotrkowandy <piotr.kowandy@o2.pl>
2019-08-07 12:22:46 +0200

    isPrime function added

--- L04-stl/12B.cpp
+++ L04-stl/12B.cpp
@@ -27,6 +27,21 @@ void fillVector(auto& v, int N, int M)
 }
 
 
+int isPrime(int n)
+{
+	int tempN = n;
+	int ch =0;
+	while (tempN > 1)
+	{
+		tempN--;
+		if (n%tempN == 0)
+		{
+			ch++;
+		}
+	}
+	return ch;
+}
+
 int main ()
 {
 	int N;
@@ -41,11 +56,36 @@ int main ()
 	std::vector<int> myVector;
 	myVector.reserve(N);
 	fillVector(myVector, N, M);
+
+	std::cout<< "myVector before any changes: ";
 	showVector(myVector);
 
 	std::cout<< "myVector after sort: ";
 	std::sort(myVector.begin(), myVector.end());
 	showVector(myVector);
+
+	std::cout<< "Removing multiply numbers. " << std::endl;
+	auto uniq = std::unique(myVector.begin(), myVector.end());
+	myVector.erase(uniq, myVector.end());
+	showVector(myVector);
+
+	std::cout<< "Erasing 0 and 1 from myVector." <<std::endl;
+	myVector.erase(std::remove(myVector.begin(), myVector.end(), 0), myVector.end());
+	myVector.erase(std::remove(myVector.begin(), myVector.end(), 1), myVector.end());
+	showVector(myVector);
+
+	myVector.shrink_to_fit();
+
+	std::cout<< "\nCreating vector with prime numbers." << std::endl;
+	std::vector<int> primeVector;
+	for (size_t i=2; i < M; i++)
+	{
+		if (isPrime(i))
+		{
+			primeVector.push_back(i);
+		}
+	}
+	showVector(primeVector);
 
 	std::map<int, std::vector<int>> primeMap;
 
