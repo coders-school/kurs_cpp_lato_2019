3bb47e77d2a6fe70f5c2b887f01f77375d0a9937
Szymon Gajewski <szymongaju@gmail.com>
2019-08-07 14:31:23 +0200

    Exercise 4 done

---
+++ L04-stl/04.cpp
@@ -0,0 +1,21 @@
+#include <list>
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+int main(){
+
+    std::list<int> ls;
+    ls.resize(1000000);
+    std::iota(ls.begin(), ls.end(), 1);
+    auto itHalf = std::next(ls.begin(), 500000);
+    std::cout << *itHalf << '\n';
+
+    std::vector<int> v;
+    v.resize(1000000);
+    std::iota(v.begin(), v.end(), 1);
+    std::cout << v[500000] << '\n';
+
+    return 0;
+}
+//result: Vector is faster
