f8cf3a7b76a3bbeb827c5f26cc327902abe58dc0
Marcin Bury <marcinburyit@gmail.com>
2019-08-07 13:10:27 +0200

    Exercise1,2

--- L04-stl/HomeWorkSTL.cpp
+++ L04-stl/HomeWorkSTL.cpp
@@ -20,9 +20,21 @@ void exercise1()
     for(auto element: ar ) {cout<<element<<" ";};               //show ar after replacment
     for(auto element: ar1) {cout<<element<<" ";}; cout<<endl;   //show ar1 after replacment
 };
+void exercise2()
+{
+    vector<int> v = {1, 2, 4, 5, 6};                                          //Ceate a vector with following values { 1, 2, 4, 5, 6 }.
+    v.erase(v.begin());                                                       //Erase the first value.
+    v.push_back(5);                                                           //Add 5 at the end.
+    v.emplace(v.begin(), 12);                                                 //create new array, type int size 10
+    cout<<"Size is: "<<v.size()<<endl<<"Max_size is : "<<v.max_size()<<endl;  //Print vector size and max_size.
+    for(auto element: v ) {cout<<element<<" ";}; cout<<endl;                  //Print vector
+    v.clear();                                                                //Clear vector
+    for(auto element: v ) {cout<<element<<" ";}; cout<<endl;                  //Print vector
+};
 int main()
 {
     exercise1();
+    exercise2();
 
     return 0;
 }
