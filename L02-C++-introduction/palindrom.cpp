dfb2a3d242551b3d24a7527bb14b40384f504941
JustynaSlazak <nadia1101@vp.pl>
2019-09-22 15:58:13 +0200

    L02_zad1

--- L02-C++-introduction/palindrom.cpp
+++ L02-C++-introduction/palindrom.cpp
@@ -2,8 +2,10 @@
 
 bool palindrom(std::string napis)
 {
-    std::size_t dlugosc = napis.size();
-    for(std::size_t i = 0; i < (dlugosc / 2); ++i)
+    //std::size_t dlugosc = napis.size();
+    //for(std::size_t i = 0; i < (dlugosc / 2); ++i)
+    float dlugosc = napis.size();
+    for(float i = 0; i< (dlugosc / 2); ++i)
     {
         if(napis[i] != napis[dlugosc - (i + 1)])
             return false;
